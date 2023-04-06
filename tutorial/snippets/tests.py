b39555d910c5afc0a7d7b1691312ed5e8a2d3dae
gr411_kaoev <oleg.krut.64@gmail.com>
2023-04-06 09:25:16 +0700

    Check pips.txt

--- tutorial/snippets/tests.py
+++ tutorial/snippets/tests.py
@@ -1,32 +1,35 @@
-from django.test import TestCase, Client
-from .models import Snippet
-
-c = Client()
-
-
-class SnippetTest(TestCase):
-    # def setUp(self):
-    #     Snippet.objects.create(created='2023,4,5',
-    #                            title='Повесть о тупейшем тесте',
-    #                            code='21',
-    #                            linenos=False,
-    #                            language='russian',
-    #                            style='fairy tale',
-    #                            owner='Sobaka Sobakovna',
-    #                            highlighted='Lorem Ipsum is simply dummy text of the printing and typesetting industry'
-    #                            )
-    #
-    # def test_snippet_contains_owner(self):
-    #     story = Snippet.objects.get(title='Повесть о тупейшем тесте')
-    #     self.assertEqual(story.contains(), 'Отрывок "Повесть о тупейшем тесте"')
-
-    def get_from_url(self):
-        c.get('http://127.0.0.1:8000/api/snippets/')
-
-        response = c.post('/api/snippets/')
-        response.content
-        response = c.get
-
+import unittest
+from rest_framework.test import *
 
 
 # Create your tests here.
+
+
+class Test(unittest.TestCase):
+    base_url = 'http://127.0.0.1:8000/api/'
+
+    def test_get_snippet(self):
+        client = APIClient()
+        client.login(username='admin', password='admin')
+        client = RequestsClient()
+        response = client.get(self.base_url + 'snippets/')
+        assert response.status_code == 200
+
+    def test_delete_snippet(self):
+        client = APIClient()
+        client.login(username='admin', password='admin')
+        response = client.delete(self.base_url + 'snippets/25/')
+        assert response.status_code == 205
+
+    def test_put_snippet(self):
+        client = APIClient()
+        client.login(username='admin', password='admin')
+        response = client.put(self.base_url + 'snippets/26/', {'code': '"<?php\r\n echo \"Greetings\""'}, format='json')
+        assert response.status_code == 200
+
+    def test_post_snippet(self):
+        client = APIClient()
+        client.login(username='admin', password='admin')
+        response = client.post(self.base_url + 'snippets/', {'code': '"<?php\r\n echo \"Hello World\""'},
+                               format='json')
+        assert response.status_code == 201
